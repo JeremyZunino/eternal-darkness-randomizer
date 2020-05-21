3dfd66faa85ab3c0f9951097cc4d1ad90c490745
Jeremy <jeremy@29ter.fr>
2020-05-21 09:14:15 +0200

    Updating test

--- extract.js
+++ extract.js
@@ -7,12 +7,10 @@ let search = "a87e0030";
 
 let dol_start = 122880;
 let dol_end   = 3279360;
+let line      = 0;
 
 
 http.createServer(function(req, res) {
-
-
-
 
 
     let stream = fs.createReadStream(file_path);
@@ -26,17 +24,25 @@ http.createServer(function(req, res) {
     });
 
     stream.on('data', data => {
-        console.log( data.length );
-        for( let i=0 ; i<data.length ; i++ ) {
-            let b = data[i].toString(16);
-            if( b === "a8" && data[i+1].toString(16) === "7e" ) {
-                console.log( i, b );
+        let i = 0;
+        while( i < data.length )
+        {
+            let instructionHex = decToHex(data[i]) + decToHex(data[i+1]) + decToHex(data[i+2]) + decToHex(data[i+3]) ;
+            if( instructionHex === "a87e0030" ) {
+                let instructionDec = parseInt( instructionHex , 16 );
+                console.log( line , instructionHex , instructionDec );
             }
+            line++;
+            i += 4;
         }
     });
 
 
-
-
-
 }).listen(8080);
+
+
+const decToHex = ( dec ) => {
+    return dec > 16
+        ? dec.toString(16)
+        : "0" + dec.toString(16);
+};
